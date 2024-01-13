1f68f8ec26c1ea2d8ac83b940944892399609000
Vituinha <56604707+Vituinha@users.noreply.github.com>
2024-01-13 15:10:00 -0300

    Adicionando o editor de texto

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Selecione uma opção dentre:");
+            Console.WriteLine("1 - Abrir arquivo\n2 - Criar novo arquivo\n0 - Sair");
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res){
+                case 0:
+                    System.Environment.Exit(0);
+                    break;
+                case 1:
+                    Abrir();
+                    break;
+                case 2:
+                    Editar();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    Menu();
+                    break;
+            }
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
+
+            using(var file = new StreamReader(path))
+            {
+                String texto = file.ReadToEnd();
+                Console.WriteLine(texto);
+            }
+
+            Console.WriteLine("");
+            Console.Read();
+            Menu();
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo: (ESC para sair)\n---------------");
+            string texto = "";
+
+            do
+            {
+                texto += Console.ReadLine();
+                texto += Environment.NewLine;
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(texto);
+        }
+
+        static void Salvar(String texto)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using(var file = new StreamWriter(path))
+            {
+                file.Write(texto);
+            }
+
+            Console.WriteLine($"Arquivo salvo com sucesso! {path}");
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
+
